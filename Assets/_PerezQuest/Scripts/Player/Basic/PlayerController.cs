587a73f68a18f51eca2ef85a54fdc8edc210bd11
ComFel <felipevm07@gmail.com>
2024-03-24 00:10:03 +0100

    continue base movement -added jump and double jump (WIP)

--- Assets/_PerezQuest/Scripts/Player/Basic/PlayerController.cs
+++ Assets/_PerezQuest/Scripts/Player/Basic/PlayerController.cs
@@ -1,56 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private bool dj;
+    [SerializeField] private int  maxJumps = 2;
+
     [SerializeField] private float movementSpeed = 5.0f;
     [SerializeField] private float jumpHeightForce = 10.0f;
-    [SerializeField] private float slideForce = 2.0f;
 
     private Rigidbody2D myRigidBody;
+    private float circleGroundedCheck = 0.6f;
+
     private bool grounded;
-    private bool sliding;
+    private int jumpsRemaining;
 
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        jumpsRemaining = maxJumps;
     }
 
     void Update()
     {
         // Comprobar si el jugador está en el suelo
-        grounded = Physics2D.OverlapCircle(transform.position, 0.2f, LayerMask.GetMask("Suelo"));
-
+        grounded = Physics2D.OverlapCircle(transform.position, circleGroundedCheck, LayerMask.GetMask("Suelo"));
+        
         // Movimiento horizontal
         float movement = Input.GetAxis("Horizontal");
         myRigidBody.velocity = new Vector2(movement * movementSpeed, myRigidBody.velocity.y);
 
         // Salto
-        if (grounded && Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && jumpsRemaining > 0)
         {
+            Jump();
+        }
+
+        //Debug.Log($"{jumpsRemaining} jumps + . {grounded}");
+    }
+
+
+    private void Jump()
+    {
+        if (grounded && !dj)
+        {
+            myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpHeightForce);
             myRigidBody.AddForce(Vector2.up * jumpHeightForce, ForceMode2D.Impulse);
         }
 
-        // Deslizarse
-        if (Input.GetButtonDown("Slide"))
+        if (dj)
         {
-            sliding = true;
-        }
-        else if (Input.GetButtonUp("Slide"))
-        {
-            sliding = false;
+            myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpHeightForce);
+            jumpsRemaining--;
         }
 
-        if (sliding)
-        {
-            myRigidBody.velocity = new Vector2(movement * slideForce, myRigidBody.velocity.y);
-        }
-
-        // Resetear la velocidad horizontal en el suelo
-        if (grounded)
-        {
-            myRigidBody.velocity = new Vector2(0f, myRigidBody.velocity.y);
-        }
+        if (grounded && dj) jumpsRemaining = maxJumps;
     }
+
 }
